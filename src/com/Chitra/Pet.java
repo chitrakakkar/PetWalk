f5af2a6cf04e9163cc62364d051476cce7b688bf
chitrakakkar <India@123>
2016-03-14 22:03:38 -0500

    Documentation

--- src/com/Chitra/Pet.java
+++ src/com/Chitra/Pet.java
@@ -2,66 +2,38 @@ package com.Chitra;
 
 import java.util.HashMap;
 
+// This project keeps track of PetVisit every day in a week where we have a chart which gives you the schedule and
+// also, if the visit exceeds the daily limit of 6, it gives out a warning.
+
 public class Pet
 {
-
+    // variables to define the class
     String Name;
     String Type;
     String Address;
-    HashMap<String,Integer> Schedule;
-
+    HashMap<String,Integer> Schedule;  // a hashmap to get the days and no of visits per day for each pet
+    // a constructor to create the object for this class
     Pet(String name, String type, String address, HashMap<String, Integer>schedule)
     {
         this.Name = name;
         this.Type = type;
-
         this.Address = address;
         this.Schedule = schedule;
     }
-//    void AddFrequency(HashMap<String,Integer> Freq)
-//    {
-//        if(Schedule == null)
-//        {
-//
-//            Schedule = new HashMap<String, Integer>();
-//        }
-//        for (String key:Freq.keySet()
-//                )
-//        {
-//            Schedule.put(key,Freq.get(key));
-//        }
-//        System.out.println(this.Name);
-//        System.out.println(this.Type);
-//        System.out.println(this.Address);
-//        System.out.println("Number of times in a week" + Schedule);
-//        System.out.println("****************************************************");
-//    }
-    public String GetKey(HashMap<String,Integer>Sched)
+    //A user defined method to print the Pet info which receives sum and WeekDays name as an argument and returns Sum
+    public Integer PrintInfo( int Sum,String ST)
     {
-        String WeekDayName=" ";
-        for (String st:Sched.keySet()
-             )
+        // prints out the info in given format
+        System.out.println(" Visit " + this.Name + " the " + this.Type + "\t" + this.Schedule.get(ST) + "\tTimes " + " Address : " + this.Address);
+        Sum = Sum + this.Schedule.get(ST); // adds the total values per day for all the pets
+        if(Sum>6) /// a condition to check if the visit is more than 6 per day
         {
-           WeekDayName = st;
+            System.out.println("*******************************************************************");
+            System.out.println("\033[0;1m"+"\033[31mWarning !!! You have more than 6 visits Today"+"\033[0m "); // to print warning in red color
 
         }
-        return WeekDayName;
+        return Sum; // it returns Sum to the called method.
     }
-    public Integer GetValue(HashMap<String,Integer>Sched)
-    {
-
-        Integer PetWalk =0;
-        for (String st:Sched.keySet()
-                )
-        {
-            PetWalk = Sched.get(st);
-
-        }
-        return PetWalk;
-    }
-
-
-
 
 }
 
--- src/com/Chitra/PetManager.java
+++ src/com/Chitra/PetManager.java
@@ -9,10 +9,13 @@ import java.util.List;
 /**
  * Created by chitrakakkar on 3/14/16.
  */
-public class PetManager {
+public class PetManager
+{
+    // A class which creates objects with Pet constructor
     public static void main(String[] args)
     {
-        List<Pet> PetList = new ArrayList<>();
+        List<Pet> PetList = new ArrayList<>();  // a list to add all the pet objects
+        // a list of all days in a week
         List<String> DaysOfWeeks = Arrays.asList("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
         // creating Pet objects with default Constructor
         String petName = "Molly";
@@ -20,7 +23,7 @@ public class PetManager {
         String petAddress = "1234 Street St.";
         // Key: Day -> Value: Number of walks
         HashMap<String, Integer> petSchedule = new HashMap<>();
-        petSchedule.put("Monday", 3);
+        petSchedule.put("Monday", 3); // adding values to the hashmap
         petSchedule.put("Tuesday", 0);
         petSchedule.put("Wednesday", 1);
         petSchedule.put("Thursday", 2);
@@ -28,11 +31,9 @@ public class PetManager {
         petSchedule.put("Saturday", 3);
         petSchedule.put("Sunday", 2);
         Pet Pet1 = new Pet(petName, petType, petAddress, petSchedule);
-        //Pet1.AddFrequency(petSchedule);
         String petName1 = "Nina";
         String petType1 = "Rabbit";
         String petAddress1 = "407 7th Street St.";
-        // Key: Day -> Value: Number of walks
         HashMap<String, Integer> petSchedule1 = new HashMap<>();
         petSchedule1.put("Monday", 2);
         petSchedule1.put("Tuesday", 1);
@@ -42,11 +43,9 @@ public class PetManager {
         petSchedule1.put("Saturday", 3);
         petSchedule1.put("Sunday", 2);
         Pet Pet2 = new Pet(petName1, petType1, petAddress1, petSchedule1);
-        //Pet2.AddFrequency(petSchedule1);
         String petName2 = "Rocky";
         String petType2 = "Dog";
         String petAddress2 = "1798, Raleigh street";
-        // Key: Day -> Value: Number of walks
         HashMap<String, Integer> petSchedule2 = new HashMap<>();
         petSchedule2.put("Monday", 3);
         petSchedule2.put("Tuesday", 0);
@@ -56,26 +55,20 @@ public class PetManager {
         petSchedule2.put("Saturday", 0);
         petSchedule2.put("Sunday", 0);
         Pet Pet3 = new Pet(petName2, petType2, petAddress2, petSchedule2);
-        //Pet3.AddFrequency(petSchedule2);
-        PetList.add(Pet1);
+        PetList.add(Pet1);  // adding pet object to the list
         PetList.add(Pet2);
         PetList.add(Pet3);
+        //for each loop to iterate over seven days in a week
         for (String ST : DaysOfWeeks)
         {
-            System.out.println(ST + ":");
-            int Sum =0;
-            for (Pet p : PetList)
+            System.out.println("\033[0;1m" + ST + ":"+ "\033[0m ");
+            int Sum =0;  // initializing sum here to reset it after every day
+            for (Pet p : PetList) // a loop to check each object
             {
-                System.out.println(" Visit " + p.Name + " the " + p.Type + "\t" + p.Schedule.get(ST) + "\tTimes " + " Address : " + p.Address);
-                Sum = Sum + p.Schedule.get(ST);
-
+                Sum = p.PrintInfo(Sum,ST); // This Sum catches the Sum sent by the method from Pet class
             }
-            if(Sum>6)
-            {
+            System.out.println("*******************************************************************");
 
-                System.out.println("Warning !!! You have more than 6 visits Today ");
-                System.out.println("**************************************************");
-            }
 
         }
     }
